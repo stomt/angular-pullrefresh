35674e156a80a9821c80fb7233933a0e3b87821e
Mathias Moeller <matze.moeller@gmail.com>
2017-05-17 12:07:01 +0200

    Revert "Fix view bug that showed 'Pull to reload' label at the end of the loading process."
    This reverts commit 14aac5cfc206dbdf98e1b46b3600594641121410.

--- dist/pullrefresh.js
+++ dist/pullrefresh.js
@@ -1,6 +1,211 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 (function () {
+  'use strict';
+
+  var PubSub = function () {
+    function PubSub() {
+      _classCallCheck(this, PubSub);
+
+      this.handlers = [];
+    }
+
+    _createClass(PubSub, [{
+      key: 'subscribe',
+      value: function subscribe(event, handler, context) {
+
+        if (typeof context === 'undefined') {
+          context = handler;
+        }
+
+        this.handlers.push({
+          event: event,
+          handler: handler.bind(context)
+        });
+
+        return this;
+      }
+    }, {
+      key: 'publish',
+      value: function publish(event) {
+        var i = 0;
+
+        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+          args[_key - 1] = arguments[_key];
+        }
+
+        for (; i < this.handlers.length; i += 1) {
+
+          if (this.handlers[i].event === event) {
+            var _handlers$i;
+
+            (_handlers$i = this.handlers[i]).handler.apply(_handlers$i, args);
+          }
+        }
+      }
+    }]);
+
+    return PubSub;
+  }();
+
+  var Pan = function () {
+    function Pan() {
+      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { resistance: 1 };
+
+      _classCallCheck(this, Pan);
+
+      this._options = options;
+
+      this.reset();
+    }
+
+    _createClass(Pan, [{
+      key: 'reset',
+      value: function reset() {
+        this._distance = 0;
+        this._start = 0;
+        this._isScrolling = false;
+        this._didMove = false;
+      }
+    }, {
+      key: 'begin',
+      value: function begin(val) {
+        this._start = val;
+      }
+    }, {
+      key: 'calcDistance',
+      value: function calcDistance(pos) {
+        return Math.round((pos - this._start) / this._options.resistance);
+      }
+    }, {
+      key: 'move',
+      value: function move(distance) {
+        this._distance = distance;
+        this._didMove = true;
+        this._isScrolling = false;
+      }
+    }, {
+      key: 'scroll',
+      value: function scroll() {
+        this._isScrolling = true;
+        this._didMove = false;
+      }
+    }, {
+      key: 'distance',
+      get: function get() {
+        return this._distance;
+      }
+    }, {
+      key: 'isScrolling',
+      get: function get() {
+        return this._isScrolling;
+      }
+    }, {
+      key: 'didMove',
+      get: function get() {
+        return this._didMove;
+      }
+    }]);
+
+    return Pan;
+  }();
+
+  var PullRefresh = function (_Pan) {
+    _inherits(PullRefresh, _Pan);
+
+    function PullRefresh() {
+      _classCallCheck(this, PullRefresh);
+
+      var _this = _possibleConstructorReturn(this, (PullRefresh.__proto__ || Object.getPrototypeOf(PullRefresh)).apply(this, arguments));
+
+      _this._el = _this._options.el;
+
+      _this._options.threshold = _this._options.threshold || 0;
+
+      _this._state = {};
+
+      _this.pubSub = new PubSub();
+
+      return _this;
+    }
+
+    _createClass(PullRefresh, [{
+      key: 'reset',
+      value: function reset() {
+        _get(PullRefresh.prototype.__proto__ || Object.getPrototypeOf(PullRefresh.prototype), 'reset', this).call(this);
+        this.resetState();
+      }
+    }, {
+      key: 'resetState',
+      value: function resetState() {
+        this._state = {};
+      }
+    }, {
+      key: 'isScrolledToTop',
+      value: function isScrolledToTop() {
+        return this._el.scrollTop === 0;
+      }
+    }, {
+      key: 'move',
+      value: function move(pos, event) {
+
+        var distance = this.calcDistance(pos);
+
+        if (!this.isScrolling && this.isScrolledToTop() && distance > 0) {
+
+          _get(PullRefresh.prototype.__proto__ || Object.getPrototypeOf(PullRefresh.prototype), 'move', this).call(this, distance);
+
+          this._state.refresh = this.distance > this._options.threshold;
+          this._state.pull = this.distance > 0;
+
+          this.pubSub.publish('moved', this);
+
+          return true;
+        } else {
+
+          this.scroll();
+
+          return false;
+        }
+      }
+    }, {
+      key: 'end',
+      value: function end() {
+
+        if (this.didMove) {
+
+          this.pubSub.publish('end', this);
+
+          this.reset();
+
+          return true;
+        }
+      }
+    }, {
+      key: 'el',
+      get: function get() {
+        return this._el;
+      }
+    }, {
+      key: 'state',
+      get: function get() {
+        return this._state;
+      }
+    }]);
+
+    return PullRefresh;
+  }(Pan);
+
   'use strict';
 
   var TOUCH_START = 'touchstart MSPointerDown pointerdown';
@@ -37,13 +242,10 @@
         // make the callback is evaluated on the parent scope
         pullrefresh: '&',
 
-        pullrefreshElement: '@',
-
-        // allow users to insert an own template for the loader
-        pullrefreshTemplate: '@'
+        pullrefreshElement: '@'
       },
 
-      templateUrl: getTemplateUrl,
+      templateUrl: 'template/pullrefresh/pullrefresh.html',
 
       link: linkPullrefreshDirective
 
@@ -51,9 +253,6 @@
 
     ///////////////////////////////////////////////////////////
 
-    function getTemplateUrl(elem, attrs) {
-      return attrs.pullrefreshTemplate || 'template/pullrefresh/pullrefresh.html';
-    }
 
     function linkPullrefreshDirective($scope, element, attrs) {
 
@@ -86,7 +285,7 @@
       /**
        * Holds all information about the current pan action
        */
-      var pan = new $window.PullRefresh({
+      var pan = new PullRefresh({
         el: ptrEl,
         threshold: options.threshold,
         resistance: options.resistance
@@ -172,6 +371,7 @@
         var t = e.touches ? e.touches[0] : e;
 
         if (pan.move(t.clientY)) {
+          e.preventDefault();
           e.stopPropagation();
         }
       }
@@ -277,225 +477,6 @@
     }
   }]);
 })();
-'use strict';
-
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-(function (window) {
-  'use strict';
-
-  var PubSub = function () {
-    function PubSub() {
-      _classCallCheck(this, PubSub);
-
-      this.handlers = [];
-    }
-
-    _createClass(PubSub, [{
-      key: 'subscribe',
-      value: function subscribe(event, handler, context) {
-
-        if (typeof context === 'undefined') {
-          context = handler;
-        }
-
-        this.handlers.push({
-          event: event,
-          handler: handler.bind(context)
-        });
-
-        return this;
-      }
-    }, {
-      key: 'publish',
-      value: function publish(event) {
-        var i = 0;
-
-        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-          args[_key - 1] = arguments[_key];
-        }
-
-        for (; i < this.handlers.length; i += 1) {
-
-          if (this.handlers[i].event === event) {
-            var _handlers$i;
-
-            (_handlers$i = this.handlers[i]).handler.apply(_handlers$i, args);
-          }
-        }
-      }
-    }]);
-
-    return PubSub;
-  }();
-
-  var Pan = function () {
-    function Pan() {
-      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { resistance: 1 };
-
-      _classCallCheck(this, Pan);
-
-      this._options = options;
-
-      this.reset();
-    }
-
-    _createClass(Pan, [{
-      key: 'reset',
-      value: function reset() {
-        this._distance = 0;
-        this._start = 0;
-        this._isScrolling = false;
-        this._didMove = false;
-        this._movedBefore = false;
-      }
-    }, {
-      key: 'begin',
-      value: function begin(val) {
-        this._start = val;
-      }
-    }, {
-      key: 'calcDistance',
-      value: function calcDistance(pos) {
-        return Math.round((pos - this._start) / this._options.resistance);
-      }
-    }, {
-      key: 'move',
-      value: function move(distance) {
-        this._distance = distance;
-        this._didMove = true;
-        this._movedBefore = true;
-        this._isScrolling = false;
-      }
-    }, {
-      key: 'scroll',
-      value: function scroll() {
-        this._isScrolling = true;
-        this._didMove = false;
-      }
-    }, {
-      key: 'distance',
-      get: function get() {
-        return this._distance;
-      }
-    }, {
-      key: 'isScrolling',
-      get: function get() {
-        return this._isScrolling;
-      }
-    }, {
-      key: 'didMove',
-      get: function get() {
-        return this._didMove;
-      }
-    }, {
-      key: 'movedBefore',
-      get: function get() {
-        return this._movedBefore;
-      }
-    }]);
-
-    return Pan;
-  }();
-
-  var PullRefresh = function (_Pan) {
-    _inherits(PullRefresh, _Pan);
-
-    function PullRefresh() {
-      _classCallCheck(this, PullRefresh);
-
-      var _this = _possibleConstructorReturn(this, (PullRefresh.__proto__ || Object.getPrototypeOf(PullRefresh)).apply(this, arguments));
-
-      _this._el = _this._options.el;
-
-      _this._options.threshold = _this._options.threshold || 0;
-
-      _this._state = {};
-
-      _this.pubSub = new PubSub();
-
-      return _this;
-    }
-
-    _createClass(PullRefresh, [{
-      key: 'reset',
-      value: function reset() {
-        _get(PullRefresh.prototype.__proto__ || Object.getPrototypeOf(PullRefresh.prototype), 'reset', this).call(this);
-        this.resetState();
-      }
-    }, {
-      key: 'resetState',
-      value: function resetState() {
-        this._state = {};
-      }
-    }, {
-      key: 'isScrolledToTop',
-      value: function isScrolledToTop() {
-        return this._el.scrollTop === 0;
-      }
-    }, {
-      key: 'move',
-      value: function move(pos, event) {
-
-        var distance = this.calcDistance(pos);
-
-        if (!this.isScrolling && this.isScrolledToTop() && distance > 0 || // check if the user pulled down
-        !this.isScrolling && !this.isScrolledToTop() && distance > 0 && this.movedBefore) {
-          // check if the user pulls up after pulling down beforehand
-
-          _get(PullRefresh.prototype.__proto__ || Object.getPrototypeOf(PullRefresh.prototype), 'move', this).call(this, distance);
-
-          this._state.refresh = this.distance > this._options.threshold;
-          this._state.pull = this.distance > 0;
-
-          this.pubSub.publish('moved', this);
-
-          return true;
-        } else {
-
-          this.scroll();
-
-          return false;
-        }
-      }
-    }, {
-      key: 'end',
-      value: function end() {
-
-        if (this.didMove) {
-
-          this.pubSub.publish('end', this);
-
-          this.reset();
-
-          return true;
-        }
-      }
-    }, {
-      key: 'el',
-      get: function get() {
-        return this._el;
-      }
-    }, {
-      key: 'state',
-      get: function get() {
-        return this._state;
-      }
-    }]);
-
-    return PullRefresh;
-  }(Pan);
-
-  window.PullRefresh = PullRefresh;
-})(window);
 'use strict';
 
 (function () {
--- src/js/directive.js
+++ src/js/directive.js
@@ -1,6 +1,186 @@
 (function() {
   'use strict';
 
+  class PubSub {
+
+    constructor () {
+      this.handlers = [];
+    }
+
+    subscribe (event, handler, context) {
+
+      if (typeof context === 'undefined') {
+        context = handler;
+      }
+
+      this.handlers.push({
+        event: event,
+        handler: handler.bind(context)
+      });
+
+      return this;
+    }
+
+    publish (event, ...args) {
+      let i = 0;
+
+      for (; i < this.handlers.length; i += 1) {
+
+          if (this.handlers[i].event === event) {
+              this.handlers[i].handler(...args);
+          }
+
+      }
+    }
+  }
+
+
+  class Pan {
+
+  constructor (options = {resistance : 1}) {
+    this._options = options;
+
+    this.reset();
+  }
+
+  get distance () {
+    return this._distance;
+  }
+
+  get isScrolling () {
+    return this._isScrolling;
+  }
+
+  get didMove () {
+    return this._didMove;
+  }
+
+  reset () {
+    this._distance = 0;
+    this._start = 0;
+    this._isScrolling = false;
+    this._didMove = false;
+  }
+
+  begin (val) {
+    this._start = val;
+  }
+
+  calcDistance (pos) {
+    return Math.round((pos - this._start) / this._options.resistance);
+  }
+
+  move (distance) {
+    this._distance = distance;
+    this._didMove = true;
+    this._isScrolling = false;
+  }
+
+  scroll () {
+    this._isScrolling = true;
+    this._didMove = false;
+  }
+
+}
+
+  class PullRefresh extends Pan {
+
+    constructor () {
+
+      super(...arguments);
+
+      this._el = this._options.el;
+
+      this._options.threshold = this._options.threshold || 0;
+
+      this._state = {};
+
+      this.pubSub = new PubSub();
+
+    }
+
+    get el () {
+      return this._el;
+    }
+
+    get state () {
+      return this._state;
+    }
+
+    reset () {
+      super.reset();
+      this.resetState();
+    }
+
+    resetState () {
+      this._state = {};
+    }
+
+    isScrolledToTop () {
+      return this._el.scrollTop === 0;
+    }
+
+    move (pos, event) {
+
+      let distance = this.calcDistance(pos);
+
+      if (!this.isScrolling &&
+        this.isScrolledToTop() &&
+        distance > 0) {
+
+        super.move(distance);
+
+        this._state.refresh = this.distance > this._options.threshold;
+        this._state.pull = this.distance > 0;
+
+        this.pubSub.publish('moved', this);
+
+        return true;
+      }
+      else {
+
+        this.scroll();
+
+        return false;
+      }
+
+    }
+
+
+    end () {
+
+      if (this.didMove) {
+
+        this.pubSub.publish('end', this);
+
+        this.reset();
+
+        return true;
+      }
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  'use strict';
+
   const TOUCH_START = 'touchstart MSPointerDown pointerdown';
   const TOUCH_MOVE = 'touchmove MSPointerMove MSPointerHover pointermove';
   const TOUCH_END = 'touchend touchcancel MSPointerUp MSPointerCancel pointerup pointercancel';
@@ -42,13 +222,10 @@
         // make the callback is evaluated on the parent scope
         pullrefresh : '&',
 
-        pullrefreshElement: '@',
-
-        // allow users to insert an own template for the loader
-        pullrefreshTemplate: '@'
+        pullrefreshElement: '@'
       },
 
-      templateUrl: getTemplateUrl,
+      templateUrl: 'template/pullrefresh/pullrefresh.html',
 
       link: linkPullrefreshDirective
 
@@ -60,9 +237,7 @@
 
     ///////////////////////////////////////////////////////////
 
-    function getTemplateUrl(elem, attrs) {
-      return attrs.pullrefreshTemplate || 'template/pullrefresh/pullrefresh.html';
-    }
+
 
     function linkPullrefreshDirective($scope, element, attrs) {
 
@@ -97,7 +272,7 @@
       /**
        * Holds all information about the current pan action
        */
-      let pan = new $window.PullRefresh({
+      let pan = new PullRefresh({
         el : ptrEl,
         threshold : options.threshold,
         resistance : options.resistance
@@ -194,6 +369,7 @@
         let t = e.touches ? e.touches[0] : e;
 
         if (pan.move(t.clientY)) {
+          e.preventDefault();
           e.stopPropagation();
         }
 
